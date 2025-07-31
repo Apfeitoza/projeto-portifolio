5524b5f50e9c100653c19c31ea2558eb88011a45
Apfeitoza <annapaulaasf@gmail.com>
2025-07-31 20:49:23 -0300

    feat: adiciona efeito visual de botão ativo no menu de navegação

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 const toggleTheme = document.getElementById("toggle-theme");
 const rootHtml = document.documentElement;
+const menuLinks = document.querySelectorAll('.menu__link');
 
+//Função para alterar a aparência de um link ativo
+menuLinks.forEach(link => {
+  link.addEventListener('click', function () {
+    
+    menuLinks.forEach(link => link.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
+
+//Função para alterar a aparencia do tema 
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
     if (currentTheme === "light") rootHtml.setAttribute("data-theme", "dark");
@@ -11,3 +22,4 @@ function changeTheme() {
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
